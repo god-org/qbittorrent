04ac45087c3b421dc17d433c47f0545a2d977058
zzzzzzqqq8 <71330272+zzzzzzqqq8@users.noreply.github.com>
2026-04-15 19:52:30 +0800

    2026-04-15 19:52:30

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,19 @@ PROFILE_PATH=/config
 CONFIG_FILE=$PROFILE_PATH/qBittorrent/config/qBittorrent.conf
 DOWNLOADS_PATH=/downloads
 
-if [ -n "${PUID:-}" ] && [ "$PUID" -ne "$(id -u "$USER_NAME")" ]; then
-  sed -i "s/^$USER_NAME:x:[0-9]*:/$USER_NAME:x:$PUID:/" /etc/passwd
-fi
+main() {
+  if [ -n "${PUID:-}" ] && [ "$PUID" -ne "$(id -u "$USER_NAME")" ]; then
+    sed -i "s/^$USER_NAME:x:[0-9]*:/$USER_NAME:x:$PUID:/" /etc/passwd
+  fi
 
-if [ -n "${PGID:-}" ] && [ "$PGID" -ne "$(id -g "$USER_NAME")" ]; then
-  sed -i "s/^\($USER_NAME:x:[0-9]*\):[0-9]*:/\1:$PGID:/" /etc/passwd
-  sed -i "s/^$USER_NAME:x:[0-9]*:/$USER_NAME:x:$PGID:/" /etc/group
-fi
+  if [ -n "${PGID:-}" ] && [ "$PGID" -ne "$(id -g "$USER_NAME")" ]; then
+    sed -i "s/^\($USER_NAME:x:[0-9]*\):[0-9]*:/\1:$PGID:/" /etc/passwd
+    sed -i "s/^$USER_NAME:x:[0-9]*:/$USER_NAME:x:$PGID:/" /etc/group
+  fi
 
-if [ ! -f "$CONFIG_FILE" ]; then
-  mkdir -p "${CONFIG_FILE%/*}"
-  cat <<EOF >"$CONFIG_FILE"
+  if [ ! -f "$CONFIG_FILE" ]; then
+    mkdir -p "${CONFIG_FILE%/*}"
+    cat <<EOF >"$CONFIG_FILE"
 [LegalNotice]
 Accepted=true
 
@@ -27,22 +28,25 @@ Connection\PortRangeMin=8999
 Downloads\SavePath=$DOWNLOADS_PATH
 Downloads\TempPath=$DOWNLOADS_PATH/temp
 EOF
-fi
-
-if [ -d "$DOWNLOADS_PATH" ]; then
-  if [ "$(stat -c %u "$DOWNLOADS_PATH")" -ne "$(id -u "$USER_NAME")" ]; then
-    chown "$USER_NAME:" "$DOWNLOADS_PATH"
   fi
-fi
 
-if [ -d "$PROFILE_PATH" ]; then
-  if [ "$(stat -c %u "$PROFILE_PATH")" -ne "$(id -u "$USER_NAME")" ]; then
-    chown -R "$USER_NAME:" "$PROFILE_PATH"
+  if [ -d "$DOWNLOADS_PATH" ]; then
+    if [ "$(stat -c %u "$DOWNLOADS_PATH")" -ne "$(id -u "$USER_NAME")" ]; then
+      chown "$USER_NAME:" "$DOWNLOADS_PATH"
+    fi
   fi
-fi
 
-if [ -n "${UMASK:-}" ]; then
-  umask "$UMASK"
-fi
+  if [ -d "$PROFILE_PATH" ]; then
+    if [ "$(stat -c %u "$PROFILE_PATH")" -ne "$(id -u "$USER_NAME")" ]; then
+      chown -R "$USER_NAME:" "$PROFILE_PATH"
+    fi
+  fi
 
-exec su-exec "$USER_NAME" qbittorrent-nox --profile="$PROFILE_PATH" "$@"
+  if [ -n "${UMASK:-}" ]; then
+    umask "$UMASK"
+  fi
+
+  exec su-exec "$USER_NAME" qbittorrent-nox --profile="$PROFILE_PATH" "$@"
+}
+
+main "$@"
